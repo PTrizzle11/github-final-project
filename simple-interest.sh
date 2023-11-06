72eec6698659af79e73f24a9ef1dd4d3314d370a
Patrick ODonnell <130483105+PTrizzle11@users.noreply.github.com>
2023-11-06 07:11:21 -0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # Ptrizzle11 (Patrick ODonnell)
    # Input:
    # p, principal amount
    # t, time period in years
